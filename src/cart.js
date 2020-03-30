f72d26ca01cbbe0366eff59416541814bf6325d5
Pete Heyes <peteheyes@Leighs-MBP-4.lan>
2020-03-30 17:06:47 +0100

    50 plus discount added

--- src/cart.js
+++ src/cart.js
@@ -3,14 +3,22 @@ let cart = {
   totalPrice: 0
 }
 
-const calculateTotalPrice = cart => {
+const calculateTotalPrice = (cart, discount) => {
   const totalPrice = cart.products.map(product => product.price * product.quantity).reduce((total, num) => total + num)
-  cart.strength ? cart.totalPrice = totalPrice * cart.strength : cart.totalPrice = totalPrice
+  const discountValue = discount ? 0.85 : 1
+
+  cart.discount = discountValue
+  cart.totalPrice = cart.strength ? (totalPrice * cart.strength)*discountValue : totalPrice*discountValue
   return cart
 }
 
+const isDiscountRequired = cart => cart.products.map(product => product.quantity)
+.reduce((total, num) => total + num) >= 50 ? true : false
+
+
 const updateCart = newCart => {
-  const newCartWithTotalPrice = calculateTotalPrice(newCart)
+  const discount = isDiscountRequired(newCart)
+  const newCartWithTotalPrice = calculateTotalPrice(newCart, discount)
   cart = newCartWithTotalPrice
   return cart
 }

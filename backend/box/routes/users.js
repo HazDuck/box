2aeab98e51ae43fdfc3579473763470868be6087
Pete Heyes <peteheyes@Leighs-MBP-4.lan>
2020-04-02 13:09:59 +0100

    added product price to order and added to the db

--- backend/box/routes/users.js
+++ backend/box/routes/users.js
@@ -6,7 +6,7 @@ router.get('/products', function(req, res) {
   var db = req.db;
   var collection = db.get('products');
   collection.find({
-    //pass in a specific document "name": "bob"
+    //can pass in a specific document "name": "bob"
   },{},function(e,docs){
     res.json(docs);
   });
@@ -19,31 +19,29 @@ router.post('/addorder', function(req, res) {
   var productsCollection = db.get('products')
   var data = req.body
 
-  //access the products information
+  //access the products information and add the correct price to the order from
+  //the db
   productsCollection.find({})
   .then(function(products) {
     const updatedData = products.map(function(product) {
       var ordersToUpdate = data.orders.filter(function(item) {
         return product.id == item.productId
       })
-
       if (!ordersToUpdate.length > 0) {
         return
       }
-      
       return Object.assign({}, ordersToUpdate[0], {price: product.price})
     })
-    return updatedData
+    return Object.assign({}, data, {orders: [...updatedData]})
   })
   .then(function(updatedData){
-    console.log(updatedData)
-    // ordersCollection.insert(updatedData, 
-    //   function(err, result) {
-    //     res.send(
-    //       (err === null) ? {msg: 'that there did a thing'} : {msg: err}
-    //     )
-    //   }
-    // )
+    ordersCollection.insert(updatedData, 
+      function(err, result) {
+        res.send(
+          (err === null) ? {msg: 'that there did a thing'} : {msg: err}
+        )
+      }
+    )
   })
   .catch(function(error) {
     console.log(error)
